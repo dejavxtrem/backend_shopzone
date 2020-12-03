f43647da7173b2aae69eecf0694eff2bdc4390e6
dejavxtrem <dejavxtrem@hotmail.com>
2020-12-02 20:03:52 -0600

    user update profile

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,6 +2,10 @@ import asyncHandler  from 'express-async-handler'
 import User from '../models/userModel.js'
 import generateToken from '../utils/generateToken.js'
 
+
+
+
+
 //@description  Get user & get token
 //@description  POST /api/users/login
 //@access       Public Routes
@@ -81,8 +85,39 @@ const getUserProfile = asyncHandler (async (req, res) => {
 })
 
 
+
+//@description  Update user prfile
+//@description  PUT /api/users/profile
+//@access       Private Routes
+const updateUserProfile = asyncHandler (async (req, res) => {
+    
+  const  user = await User.findById(req.user._id)
+  if (user) {
+      user.name = req.body.name || user.name
+      user.email = req.body.email || user.email
+      if(req.body.password) {
+        user.password = req.body.password
+      }
+      const updatedUser = await user.save()
+      res.json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+        token:  generateToken(updatedUser._id),
+      })
+
+  } else {
+      res.status(404)
+      throw new Error('User not found')
+  }
+
+})
+
+
 export {
     authUser,
     getUserProfile,
-    registerUser
+    registerUser,
+    updateUserProfile
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { authUser, getUserProfile, registerUser} from '../controllers/userControllers.js'
+import { authUser, getUserProfile, registerUser, updateUserProfile} from '../controllers/userControllers.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 const userAuthRouter = express.Router()
@@ -7,7 +7,11 @@ const userAuthRouter = express.Router()
 
 userAuthRouter.route('/').post(registerUser)
 userAuthRouter.post('/login', authUser)
-userAuthRouter.route('/profile').get(protect, getUserProfile)
+
+userAuthRouter.
+       route('/profile')
+       .get(protect, getUserProfile)
+       .put(protect, updateUserProfile)
 
 
 
